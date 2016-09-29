b434e09b40a69d2a2d2e357c54e6c5a157b4a8a5
ambikavohra136 <ambika.vohra136@gmail.com>
2016-09-29 14:45:01 -0400

    Implemented Ninety_Nine_Bottles_of_Beer function to print out full song

--- BeerSong.java
+++ BeerSong.java
@@ -21,6 +21,19 @@ public class BeerSong {
     
   public static void Ninety_Nine_Bottles_of_Beer()
   {
+  	int start = 99;
+      
+      for (int i = start; i >= 1; i--)
+      {
+          if (i==1)
+          {
+              System.out.println("" +i+ " bottles of beer on the wall, " +i+ " bottles of beer" +"\n" +
+"take one down, pass it around, no more bottles of beer on the wall.");
+              break;
+          }
+          System.out.println("" +i+ " bottles of beer on the wall, " +i+ " bottles of beer" +"\n" +
+"take one down, pass it around, " +(i-1)+ " bottles of beer on the wall.");
+      }
       
   }
     
